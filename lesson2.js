7279cf637306f73cbfc87abd78d3121b78c74722
Dima <dmitrijs.visnakovs@inbox.lv>
2023-11-21 09:09:41 +0200

    Lesson2 - Alert Prompt Confirm

---
+++ lesson2.js
@@ -0,0 +1,17 @@
+// alert prompt confirm
+//==============
+// alert('Hello World');
+
+
+// Пример с prompt
+//==============
+// let yourName = prompt("What is your name");
+
+// console.log(yourName);
+
+
+// Пример с comfirm
+//==============
+// let isBoss = confirm("Ты здесь главный?");
+
+// console.log(isBoss);
