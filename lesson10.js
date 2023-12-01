207f56d2baea1b8252ade41f85c8c26073c374df
Dima <dmitrijs.visnakovs@inbox.lv>
2023-12-01 08:11:49 +0200

    Switch

---
+++ lesson10.js
@@ -0,0 +1,47 @@
+// Тема: Switch
+
+let dayType;
+
+let day = '1';
+
+switch(day) {
+    case "Monday":
+    case 'Tuesday':
+    case 'Wednesday':
+    case 'Thursday':
+    case 'Friday':
+        dayType = 'Weekday';
+        break;
+    case 'Saturday':
+    case 'Sunday':
+        dayType = 'Weekend';
+        break;
+    default:
+        dayType = 'Unknown';
+}
+
+console.log(dayType);
+
+
+
+//============= 
+
+let type;
+
+let number = -2;
+
+switch (true) {
+  case number > 0:
+    type = 'Positive';
+    break;
+  case number < 0:
+    type = 'Negative';
+    break;
+  case number === 0:
+    type = 'Zero';
+    break;
+  default:
+    type = 'NaN';
+}
+
+console.log(type);
