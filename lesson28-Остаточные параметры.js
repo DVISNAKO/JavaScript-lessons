9e0778f61456415037f38e2b3cb248e6e8640880
Dima <dmitrijs.visnakovs@inbox.lv>
2024-01-05 08:30:59 +0200

    Residual parameters

---
+++ lesson28-Остаточные параметры.js
@@ -0,0 +1,50 @@
+// Остаточные параметры и оператор расширения
+/*Остаточные параметры используются, чтобы создавать 
+новые функции с неопределённым числом аргументов.*/
+
+function sumAll(...args) { // args — имя массива
+    let sum = 0;
+  
+    for (let arg of args) sum += arg;
+  
+    return sum;
+  }
+  
+console.log( sumAll(1) ); // 1
+console.log( sumAll(1, 2) ); // 3
+console.log( sumAll(1, 2, 3) ); // 6
+
+//
+
+function showName(firstName, lastName, ...titles) {
+  console.log( firstName + ' ' + lastName ); // Юлий Цезарь
+
+  // Оставшиеся параметры пойдут в массив
+  // titles = ["Консул", "Император"]
+  console.log( titles[0] ); // Консул
+  console.log( titles[1] ); // Император
+  console.log( titles.length ); // 2
+}
+
+showName("Юлий", "Цезарь", "Консул", "Император");
+
+//============
+
+
+let arr = [3, 5, 1];
+
+console.log( Math.max(arr) ); // NaN
+
+// ------------- добавить и обычное число
+let arr1 = [1, -2, 3, 4];
+let arr2 = [8, 3, -8, 1];
+
+console.log( Math.max(1, ...arr1, 2, ...arr2, 25) ); // 25
+
+// слияние массивов
+let arr3 = [3, 5, 1];
+let arr4 = [8, 9, 15];
+
+let merged = [0, ...arr3, 2, ...arr4];
+
+console.log(merged);
