a3ae578ea090f9f4750850ec85793de9b13ef209
Dima <dmitrijs.visnakovs@inbox.lv>
2024-01-08 11:39:36 +0200

    closure

---
+++ lesson29-Область видимости переменных, замыкание.js
@@ -0,0 +1,74 @@
+//Тема: Область видимости переменных, замыкание
+
+{
+let message = "Hello";
+console.log(message);
+}
+// показать другое сообщение
+{
+let message = "Goodbye"; // SyntaxError: Identifier 'message' has already been declared
+console.log(message);
+}
+
+//=============
+
+//Для if, for, while и т.д. переменные, видны только внутри
+if (true) {
+    let phrase = "Hello";
+  
+    console.log(phrase); // Hello
+  }
+  
+ console.log(phrase); // ошибка phrase is not defined
+
+
+
+ 
+//============= Вложенные функции
+//Функция называется «вложенной», когда она создаётся внутри другой функции.
+
+ function makeCounter() {
+    let count = 0;
+  
+    return function() {
+      return count++; // есть доступ к внешней переменной "count"
+    };
+  }
+  
+  let counter = makeCounter();
+  
+   console.log( counter() ); // 0
+   console.log( counter() ); // 1
+   console.log( counter() ); // 2
+
+   /*вопрос: «что такое замыкание?»
+   Замыкание – это функция, которая запоминает свои внешние переменные и может получить к ним доступ. 
+   в JavaScript, все функции изначально являются замыканиями.
+   */
+
+
+let name = "John";
+
+function sayHi() {
+console.log("Hi, " + name);
+}
+
+name = "Pete";
+
+sayHi(); // что будет показано: "John" или "Pete"?
+
+
+//====
+
+function first() {
+let number = 1;
+
+function second () {
+    let number = 2;
+    console.log((number));
+}
+return second;
+}
+
+const showResult = first();
+showResult(); //будет показано: 2
